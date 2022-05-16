2ae17842c3c6378dc2fb675e29aefffd8ad40f8e
yechan6527 <andy6527@naver.com>
2022-05-16 23:02:39 +0900

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <div>
+      <div class="mx-4 text-4xl font-bold">검색</div>
+       <div class=" bg-gray-200 rounded mx-4">
+       <img src="/g돋보기.png" alt="g"/>
+       
+        <div class="text-gray-400 text-xl mx-6">게임, 앱, 스토리 등</div>
+       </div>
+       
+       <div class=" text-2xl my-10 mx-2">
+         <div class="mx-2 font-semibold">새로운 발견</div>
+       <div class="text-blue-400 text-2xl m-2">
+       <div class="border-b border-gray-00"></div>
+         <div>주식</div>
+         <div class="border-b border-gray-00"></div>
+         <div>비행기</div>
+         <div class="border-b border-gray-00"></div>
+         <div>소음측정기</div>
+         <div class="border-b border-gray-00"></div>
+         <div>버블</div>
+         
+         
+       </div>
+         
+       </div>
+       <div class="mx-4 text-2xl font-semibold">추천 앱과 게임</div>
+       <div class="border-b border-gray-00 mx-4 my-2"></div>
+       <div class="mx-4 my-2 bg-blue-100 rounded">
+         <div class="mx-16">
+           <div class="text-2xl">쿠팡 (Coupang)</div>
+         <div class="text-gray-400">스마트한 쇼핑을 하기 위한...</div>
+         <div class="bg-blue-300 w-6 rounded">
+           <div class="text-xs my-2 text-white">광고</div>
+           </div>
+         </div>
+       </div>
+       <div class="mx-20 my-2">
+         <div class="text-2xl">피망 뉴맞고:</div>
+         <div class="text-2xl">고스톱으로 대한민국 1...</div>
+         <div class="text-gray-400">대한민국 1등 고스톱, 피망...</div>
+         
 
-export default App;
+       </div>
+
+    </div>
+  )
+}
